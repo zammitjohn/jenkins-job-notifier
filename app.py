c9a1c776ab592208693335d5cdd99595287ac25d
zammitjohn <zammitjohn@icloud.com>
2023-03-24 16:49:32 +0100

    Improve Several Builds notification

--- app.py
+++ app.py
@@ -102,8 +102,8 @@ def get_jenkins_builds() -> any:
         return reversed(response.json()['builds'])
     except requests.exceptions.HTTPError as error:
         logging.error("Failed to fetch data: %s", str(error))
-        notify('Jenkins API data fetch failed', 
-              'Failed to fetch data from Jenkins API. Please check the log file for more information.')
+        notify('Error retrieving ' + JENKINS_JOB_NAME + ' data from Jenkins', 
+              'Failed to fetch data from Jenkins API. Please check the log file for more information and ensure that Jenkins is accessible.')
         sys.exit()
 
 def get_build_relative_time(build: any) -> float:
@@ -144,23 +144,30 @@ def build_is_today(build: any) -> bool:
 
 async def check_builds() -> None:
     """
-    Polls the Jenkins API for build status and sends notifications for long-running, timed out and failed builds.
-    The function sleeps for a fixed interval of BUILD_POLL_FREQUENCY_SECONDS between each API request and runs 
-    indefinitely until it is stopped externally.
+    Polls the Jenkins API for build status and sends notifications for long-running and running, timed out and 
+    failed builds. The function sleeps for a fixed interval of BUILD_POLL_FREQUENCY_SECONDS between each API 
+    request and runs indefinitely until it is stopped externally.
     """
     display_names_failed = []
-    ids_ignore = []        
+    ids_checked = []
+    hashes_running = []
+                    
     while True:
         logging.info("Checking builds")
+        display_names_running = []
 
         for build in get_jenkins_builds():
-            if build['id'] not in ids_ignore:
+
+            if bool(build['building']):
+                display_names_running.append(build['fullDisplayName'])
+
+            if build['id'] not in ids_checked:
 
                 build_relative_time = get_build_relative_time(build)
 
                 if build['result'] == "FAILURE":
                     display_names_failed.append(build['fullDisplayName'])
-                    ids_ignore.append(build['id'])
+                    ids_checked.append(build['id'])
                     build_failed_count = display_names_failed.count(build['fullDisplayName'])
 
                     if build_failed_count >= MAX_FAILED_BUILD_ATTEMPTS and build_is_today(build):
@@ -169,37 +176,46 @@ async def check_builds() -> None:
                             build['id'])
 
                 elif build_relative_time >= MAX_RUNNING_BUILD_DURATION_SECONDS and bool(build['building']):
-                    ids_ignore.append(build['id'])
+                    ids_checked.append(build['id'])
                     notify('Build still running',
                         build['fullDisplayName'] + " has been running for the last " + str(round(float(build_relative_time/3600), 1)) + " hours.",
                         build['id'])
 
                 elif build['duration']/1000 >= MAX_ABORTED_BUILD_DURATION_SECONDS and build['result'] == "ABORTED" and build_is_today(build):
-                    ids_ignore.append(build['id'])
+                    ids_checked.append(build['id'])
                     notify('Build has timed out',
                         build['fullDisplayName'] + " aborted after " + str(round(float(build['duration']/3.6e+6), 1)) + " hours.",
-                        build['id'])                                      
+                        build['id'])          
+        
+        """ 
+        the following creates a hash of a list of display names for running builds, compares it to a list 
+        of existing hashes, and sends a notification if the hash is not already in the list and the number
+        of running builds exceeds the defined maximum value. 
+        """
+        builds_running_hash = hash(str(display_names_running))
+        builds_running_count = len(display_names_running)
+        if builds_running_hash not in hashes_running:
+            hashes_running.append(builds_running_hash)
+            if builds_running_count >= MAX_RUNNING_BUILDS:
+                notify('Several ' + JENKINS_JOB_NAME + ' builds running',
+                    str(builds_running_count) + " builds currently running: " + ', '.join(display_names_running))
 
         await asyncio.sleep(BUILD_POLL_FREQUENCY_SECONDS)
         
 async def check_job() -> None:
     """
-    Polls the Jenkins API for job status and sends notifications according to the number of running, aborted, 
-    executed, and failed builds. The function sleeps for a fixed interval of JOB_POLL_FREQUENCY_SECONDS between
-    each API request. The function runs indefinitely until it is stopped externally.
+    Polls the Jenkins API for job status and sends notifications according to the number of aborted, executed, 
+    and failed builds. The function sleeps for a fixed interval of JOB_POLL_FREQUENCY_SECONDS between each API 
+    request. The function runs indefinitely until it is stopped externally.
     """    
     while True:
         logging.info("Checking job")
-        count_running_builds = 0
         count_aborted_builds = 0
         count_failed_builds = 0
         count_executed_builds = 0
 
         for build in get_jenkins_builds():
             build_relative_time = get_build_relative_time(build)
-
-            if bool(build['building']):
-                count_running_builds += 1
 
             if build_relative_time <= JOB_POLL_FREQUENCY_SECONDS:
                 if build['result'] == "ABORTED":
@@ -209,20 +225,16 @@ async def check_job() -> None:
                 elif bool(build['building']):
                     count_executed_builds += 1          
 
-        if count_running_builds >= MAX_RUNNING_BUILDS:
-            notify('Several builds running',
-                str(count_running_builds) + " builds currently running.")
-
         if count_aborted_builds >= MAX_ABORTED_BUILDS:
-            notify('Several builds aborted',
+            notify('Several ' + JENKINS_JOB_NAME + ' builds aborted',
                 str(count_aborted_builds) + " builds aborted within the last " + str(round(float(JOB_POLL_FREQUENCY_SECONDS/3600), 1)) + " hours.")
             
         if count_failed_builds >= MAX_FAILED_BUILDS:
-            notify('Several builds failed',
+            notify('Several ' + JENKINS_JOB_NAME + ' builds failed',
                 str(count_failed_builds) + " builds failed within the last " + str(round(float(JOB_POLL_FREQUENCY_SECONDS/3600), 1)) + " hours.")
             
         if count_executed_builds >= MAX_EXECUTED_BUILDS:
-            notify('Several builds executed',
+            notify('Several ' + JENKINS_JOB_NAME + ' builds executed',
                 str(count_executed_builds) + " builds executed within the last " + str(round(float(JOB_POLL_FREQUENCY_SECONDS/3600), 1)) + " hours.")
         
         await asyncio.sleep(JOB_POLL_FREQUENCY_SECONDS)
